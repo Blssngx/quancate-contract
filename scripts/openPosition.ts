0480330ef23ee2c1b398b02ed95f3819240fbcec
Blessing Tanyaradzwa Hove <86293408+Blssngx@users.noreply.github.com>
2023-11-26 11:27:09 +0200

    Update package name and fix code formatting

--- scripts/openPosition.ts
+++ scripts/openPosition.ts
@@ -1,53 +1,3 @@
-// import {ethers} from "hardhat";
-// import {IPositionRouter} from "../typechain-types";
-
-// async function main() {
-//   const positionRouterAddress = "0x86869a5328eCb98b938A71AaC450eABC5E06b242";
-//   const positionRouter = await ethers.getContractAt("IPositionRouter", positionRouterAddress) as any as IPositionRouter;
-//   const executionFee = await positionRouter.minExecutionFee();
-//   console.log("executionFee: ", executionFee.toString())
-
-//   // create position example
-//   // long BTC with WETH (0.005) with 2x leverage and use cUSD as collateral
-//   const wbtcAddress = "0xd71Ffd0940c920786eC4DbB5A12306669b5b81EF" //wbtc
-//   const collateral = "0x471EcE3750Da237f93B8E339c536989b8978a438" //celo
-
-//   const path = [collateral, wbtcAddress]
-//   const indexToken = wbtcAddress
-//   const amountIn = "14900000000000000000" // 15 CELO
-//   const minOut = 0
-//   const sizeDelta = "20423653259033587500000000000000" //USD value of position with the leverage
-//   const isLong = true
-//   const acceptablePrice =  "37600583670773750000000000000000000"
-//   const referralCode = "0x0000000000000000000000000000000000000000000000000000000000000000"
-//   const callbackTarget = "0x0000000000000000000000000000000000000000"
-
-//   const tx = await positionRouter.createIncreasePosition(
-//     path,
-//     indexToken,
-//     amountIn,
-//     minOut,
-//     sizeDelta,
-//     isLong,
-//     acceptablePrice,
-//     executionFee,
-//     referralCode,
-//     callbackTarget,
-//     {value: executionFee}
-//   )
-//   await tx.wait()
-//   console.log("tx: ", tx.hash);
-
-
-// }
-
-// // We recommend this pattern to be able to use async/await everywhere
-// // and properly handle errors.
-// main().catch((error) => {
-//   console.error(error);
-//   process.exitCode = 1;
-// });
-
 import { ethers } from "hardhat";
 import { IPositionRouter } from "../typechain-types";
 import { IVault } from "../typechain-types";
@@ -69,8 +19,8 @@ async function main() {
   // Fetch data from localhost:5000/signals/(indexToken)
   const indexToken = "0xd71Ffd0940c920786eC4DbB5A12306669b5b81EF"; // Replace with the actual index token
   try {
-    // const maxPrice = await vault.getMaxPrice(indexToken);
-    // console.log("maxPrice: ", maxPrice.toString());
+    const maxPrice = await vault.getMaxPrice(indexToken);
+    console.log("maxPrice: ", maxPrice.toString());
     const response = await axios.get(`http://localhost:5000/signal/${indexToken}`);
     const signalData = response.data[0];
     console.log("Signal data: ", response.data);
@@ -106,8 +56,8 @@ async function main() {
     const amountIn = "4000000000000000000"; // 15 CELO
     const minOut = 0;
     const sizeDelta = "20423653259033587500000000000000"; // USD value of position with the leverage
-    const isLong = false;
-    const acceptablePrice = "37664430000000000000000000000000000";
+    const isLong = true;
+    const acceptablePrice = maxPrice;
     const referralCode =
       "0x0000000000000000000000000000000000000000000000000000000000000000";
     const callbackTarget =
